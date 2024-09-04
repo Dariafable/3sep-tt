ef3176c01782d94334f6c6ab7cb12ff46dbf7e18
Daria Nebylitsa <daranebylitsa@gmail.com>
2024-09-04 13:28:09 +0200

    changed OrderPage to arrow func

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,11 +1,13 @@
 import { cookies } from "next/headers";
 import { OrderList } from "@features/order-list/components";
 
-export default async function OrdersPage() {
+const OrdersPage = async () => {
   const cookieStore = cookies();
   const pickupData = cookieStore.get("pickupData")?.value;
 
   const parsedData = pickupData ? JSON.parse(pickupData) : null;
 
   return <OrderList pickupData={parsedData} />;
-}
+};
+
+export default OrdersPage;
